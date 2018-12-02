1f1771658eddc6bad2dbe348b10aa74ddb859a05
Andre-Tobois <tobois.andre@gmail.com>
2018-12-01 19:08:39 -0500

    feat: minor change.

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/index.html', (req, res) => {
-  res.json("app");
-});
-
-app.get('/jamsine', (req, res) => {
-  res.json("jasmine");
-});
-
-
+// Set port
 const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log("app is running");
+});
